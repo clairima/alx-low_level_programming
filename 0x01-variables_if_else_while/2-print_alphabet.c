38d7456fd5ef20e79f71b826e73aeba8a6eab9e0
clairima <mertahrima@gmail.com>
2023-03-17 15:33:03 -0700

    alphabet2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,15 +8,16 @@
  * Return: Always 0 (Success)
 */
 
-int main()
+int main(void)
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
 	{
 		putchar(letter);
-	        letter++;
+		letter++;
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
 
