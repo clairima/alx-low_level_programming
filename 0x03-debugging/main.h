7c888da7471e56671a9bd8371e95feb7c340b598
clairima <mertahrima@gmail.com>
2023-03-23 03:23:18 -0700

    debagging 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,6 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include "~/alx-low_level_programming/0x01-variables_if_else_while/positive_or_negative.c"
 
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
