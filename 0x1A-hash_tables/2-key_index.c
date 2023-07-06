2698a79b725d361e0fd911f39793d54bacff3a4d
clairima <mertahrima@gmail.com>
2023-07-06 13:27:40 -0700

    1 djb2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+/**
+ * key_index - Calculates the index of a key in a hash table array
+ * @key: The key as an unsigned character array
+ * @size: The size of the array of the hash table
+ *
+ * Return: The index at which the key/value pair should be stored
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash = hash_djb2(key);
+	unsigned long int index = hash % size;
+	return (index);
+}
