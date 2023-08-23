6eb9bcdc197f63654ff4063924123fc81f0408c7
clairima <mertahrima@gmail.com>
2023-08-23 16:10:50 -0700

    4 5 makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+5-island_perimeter.py
+"""
+
+def island_perimeter(grid):
+        """
+            Calculates the perimeter of the island described in grid.
+
+                Args:
+                        grid (list): List of list of integers representing the island.
+
+                            Returns:
+                                    int: Perimeter of the island.
+                                        """
+                                            perimeter = 0
+                                                rows, cols = len(grid), len(grid[0])
+
+                                                    for row in range(rows):
+                                                                for col in range(cols):
+                                                                                if grid[row][col] == 1:
+                                                                                                    perimeter += 4
+                                                                                                                    if row > 0 and grid[row - 1][col] == 1:
+                                                                                                                                            perimeter -= 2
+                                                                                                                                                            if col > 0 and grid[row][col - 1] == 1:
+                                                                                                                                                                                    perimeter -= 2
+
+                                                                                                                                                                                        return perimeter
+
+                                                                                                                                                                                    if __name__ == "__main__":
+                                                                                                                                                                                            grid = [
+                                                                                                                                                                                                            [0, 0, 0, 0, 0, 0],
+                                                                                                                                                                                                                    [0, 1, 0, 0, 0, 0],
+                                                                                                                                                                                                                            [0, 1, 0, 0, 0, 0],
+                                                                                                                                                                                                                                    [0, 1, 1, 1, 0, 0],
+                                                                                                                                                                                                                                            [0, 0, 0, 0, 0, 0]
+                                                                                                                                                                                                                                                ]
+                                                                                                                                                                                                print(island_perimeter(grid))
+
