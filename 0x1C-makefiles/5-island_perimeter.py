69e391b2b1f33f7363f1c29920b39a265d96b663
clairima <mertahrima@gmail.com>
2023-08-26 02:24:31 -0700

    100 make

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -23,20 +23,8 @@ def island_perimeter(grid):
             if grid[row][col] == 1:
                 perimeter += 4
                 if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2:
+                    perimeter -= 2
                 if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2:
+                    perimeter -= 2
 
     return perimeter
-
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-
-    print(island_perimeter(grid))
